1fe784ed37f91d433892b7470dd60cde02aaccd5
paul <136331@qq.com>
2022-09-25 16:23:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author paul
+ * @create 2022-09-25 16:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
